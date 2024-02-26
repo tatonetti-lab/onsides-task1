81200640a74fa21ec0e37b56c2d189123cefbc08
undinag <undina@bu.edu>
2024-02-26 16:13:15 -0500

    updated evaluation

--- evaluation_functions.py
+++ evaluation_functions.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from common_string import common_lenient_performance
+from sentence_transformers.util import cos_sim
 
-def evaluation_subtype(manual_ades, gpt_output, drug, section='adverse reactions', subtype = 'all', lenient=False):
-    '''
-    For a given drug, evaluate the performance of GPT on a given subtype of ADEs. 
-    '''
+def get_manual_data(manual_ades, drug, section='adverse reactions', subtype='all'):
+    """
+    Get the subset of the manual annotation appropriate for the evaluation
+    """
 
     if not section in ('adverse reactions', 'boxed warnings', 'warnings and precautions'):
         raise Exception(f"Unexpected section, {section}, provided.")
@@ -24,9 +25,37 @@ def evaluation_subtype(manual_ades, gpt_output, drug, section='adverse reactions
         drug_df = drug_df[drug_df.discontinuous_term == 1]
     else:
         raise Exception(f"Unexpected subtype, {subtype}, provided.")
-
     
-    manual = set(drug_df['reaction_string'].to_list())
+
+    return set(drug_df['reaction_string'].to_list())
+    
+def embed_evaluation(man_df, gpt_embeds, threshold = 0.6681796):
+    TP, FN, FP = 0, 0, 0
+    for (term, embed) in man_df.items():
+        sims = [float(cos_sim(embed, gpt_emb)) for gpt_emb in gpt_embeds]
+        try:
+            if np.max(sims) > threshold:
+                TP += 1
+            else:
+                FN += 1
+        except:
+            raise Exception(len(gpt_emb), man_df.keys(), len(sims))
+    
+    for gpt_emb in gpt_embeds:
+        sims = [float(cos_sim(gpt_emb, man_emb)) for (term, man_emb) in man_df.items()]
+        try:
+            if np.max(sims) < threshold:
+                FP += 1
+        except:
+            raise Exception(len(gpt_emb), man_df.keys(), len(sims))
+
+    return TP, FN, FP
+
+def evaluation_subtype(manual, gpt_output, drug, section='adverse reactions', subtype='all', eval_method='strict'):
+    '''
+    For a given drug, evaluate the performance of GPT on a given subtype of ADEs. 
+    '''
+
     gpt_drug = (gpt_output[
         (gpt_output['drug_name'] == drug)
         &
@@ -43,11 +72,21 @@ def evaluation_subtype(manual_ades, gpt_output, drug, section='adverse reactions
     except:
         return [drug, section, subtype, len(manual), len(gpt_drug), np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
         
-    if not lenient:    
-        #overall
+    if eval_method == 'strict':    
         TP = len(manual.intersection(gpt_drug))
         FP = len(gpt_drug.difference(manual))
         FN = len(manual.difference(gpt_drug))
+    elif eval_method == 'lenient':
+        [TP, FP, FN] = common_lenient_performance(gpt_drug, manual)
+    elif eval_method == 'embed':
+        [TP, FP, FN] = [0,0,0]
+    
+    if subtype != 'all':
+            # these can't be computed for the subtypes
+            precision = np.nan
+            f1 = np.nan
+            FP = np.nan
+    else:
         if TP == 0 and FP == 0:
             precision = np.NAN
         else:
@@ -60,52 +99,41 @@ def evaluation_subtype(manual_ades, gpt_output, drug, section='adverse reactions
             f1 = (2 * precision * recall)/(precision + recall)# 2*TP/(2*TP+FP+FN)
         else:
             f1 = np.NAN
-    else:
-        [TP, FP, FN, precision, recall, f1] = common_lenient_performance(gpt_drug, manual)
-    
-    if subtype != 'all':
-            # these can't be computed for the subtypes
-            precision = np.nan
-            f1 = np.nan
-            FP = np.nan
     
     return [drug, section, subtype, len(manual), len(gpt_drug), TP, FP, FN, precision, recall, f1]
 
-def evaluation(manual_ades, gpt_output, lenient=False, limit = 1000):
+def evaluation_granular(manual_ades, gpt_output, eval_method='strict', embed_model=None):
     drugs = gpt_output['drug_name'].unique()
-    drugs_set = set()
     results = []
-    for drug in tqdm(drugs):
-            results.append(evaluation_subtype(manual_ades, gpt_output, drug, lenient))        
-    results = pd.DataFrame(results, columns=['drug_name', 'exclude', 'n_manual', 'n_gpt', 'tp', 'fp', 'fn', 'precision', 'recall', 'f1'])
-    return results
 
-def evaluation_granular(manual_ades, gpt_output, limit = 1000, lenient=False):
-    drugs = gpt_output['drug_name'].unique()
-    drugs_set = set()
-    results = []
     for drug in tqdm(drugs):
-        drugs_set.add(drug)
-        if len(drugs_set) > limit:
-            break
-        
         for section in ['adverse reactions', 'warnings and precautions','boxed warnings']:
             for subtype in ['all', 'exact-meddra', 'non-meddra', 'negated', 'discontinuous']:
-                results.append(evaluation_subtype(manual_ades, gpt_output, drug, section, subtype, lenient))
+                manual_data = get_manual_data(manual_ades, drug, section=section, subtype=subtype)
+                gpt_data = subset_gpt_data(gpt_output, drug, section=section, subtype=subtype, eval_method=eval_method)
+                if manual_data.shape == 0:
+                    continue
+                if eval_method == 'embed':
+                    man_embeds = dict(zip(drug_df['reaction_string'], drug_df['embeds']))
+                results.append(evaluation_subtype(manual_data, gpt_output, eval_method=eval_method))
 
     results = pd.DataFrame(results, columns=['drug_name', 'section', 'ade_type', 'n_manual', 'n_gpt', 'tp', 'fp', 'fn', 'precision', 'recall', 'f1'])
     return results
 
-def evaluate(outputs, manual_ades, eval_method='strict'):
+def evaluate(outputs, manual_ades, eval_method='strict', embed_model_name=None):
     print(f"Running {eval_method} evaluation and saving results to disk.")
 
     for run_key, output in outputs.items():
-        granular_save_filename = 'results/{}_{}_granular.csv'.format(run_key, eval_method)
-        overall_save_filename = 'results/{}_{}_overall.csv'.format(run_key, eval_method)
+        if eval_method != 'embed':
+            granular_save_filename = 'results/{}_{}_granular.csv'.format(run_key, eval_method)
+            overall_save_filename = 'results/{}_{}_overall.csv'.format(run_key, eval_method)
+        else:
+            granular_save_filename = 'results/{}_{}_granular.csv'.format(run_key, embed_model_name)
+            overall_save_filename = 'results/{}_{}_overall.csv'.format(run_key, embed_model_name)
         
         print(run_key)
         
-        results_granular = evaluation_granular(manual_ades, output, lenient=(eval_method=='lenient'))
+        results_granular = evaluation_granular(manual_ades, output, eval_method=eval_method, embed_model = embed_model)
         overall_results = results_granular.groupby(['section','ade_type'])[['tp', 'fp', 'fn']].sum(min_count = 1).reset_index()
         overall_results['micro_precision'] = overall_results['tp']/(overall_results['tp']+overall_results['fp'])
         overall_results['micro_recall'] = overall_results['tp']/(overall_results['tp']+overall_results['fn'])
